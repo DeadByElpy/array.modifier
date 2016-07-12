258c79a9926725ea47eca3af66e96928d0d153fe
igor <deadbyelpy@gmail.com>
2016-07-12 10:07:57 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function filterPush ( dataArray, updateArray, mixedCallback ) {
+    if ( updateArray && updateArray.constructor.name === 'Array' ) {
+        if ( typeof mixedCallback === 'function' ) {
+            dataArray.forEach(function ( item, itemIndex ) {
+                var removeIndex = null;
+                updateArray.forEach(function ( newItem, index ) {
+                    if ( mixedCallback(item, newItem) ) {
+                        removeIndex = index;
+                    }
+                });
+
+                if ( removeIndex !== null ) {
+                    dataArray[itemIndex] = updateArray[removeIndex];
+                    updateArray.splice(removeIndex, 1);
+                }
+            });
+        }
+
+        dataArray = dataArray.concat(updateArray);
+    } else {
+        dataArray = dataArray.filter(mixedCallback);
+    }
+
+    return dataArray;
+}
